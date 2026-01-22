1e2fce72ca104a3443c3de4f8ab91fdbd799650b
SyedMuhammadIzaan <muhammadizaan258@gmail.com>
2026-01-22 23:11:13 +0500

    define the base route for product api

--- src/api/product.api.js
+++ src/api/product.api.js
@@ -1,25 +1,25 @@
 import axiosInstance from "./axiosInstance";
 
 export const createProduct=(productData,file)=>{
-    axiosInstance.post(productData,file)
+    axiosInstance.post('/product/c',productData,file)
 }
 
 export const getProductById=(id)=>{
-    axiosInstance.get(id);
+    axiosInstance.get(`/product/${id}`);
 }
 
 export const getAllProduct=()=>{
-    axiosInstance.get();
+    axiosInstance.get('/product/');
 }
 
 export const updateProduct=(id,productData,file)=>{
-    axiosInstance.update(id,productData,file);
+    axiosInstance.update(`/product/${id}`,productData,file);
 }
 
 export const deleteProdutById=(id)=>{
-    axiosInstance.delete(id);
+    axiosInstance.delete(`/product/${id}`);
 }
 
 export const deleteAllProduct=()=>{
-    axiosInstance.delete();
+    axiosInstance.delete('/product');
 }
