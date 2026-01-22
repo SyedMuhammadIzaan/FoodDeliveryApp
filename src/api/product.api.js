7b093b84d48d64db7f3dcb530955c5fae4d3bff2
SyedMuhammadIzaan <muhammadizaan258@gmail.com>
2026-01-22 22:57:53 +0500

    create a product api

--- src/api/product.api.js
+++ src/api/product.api.js
@@ -3,3 +3,23 @@ import axiosInstance from "./axiosInstance";
 export const createProduct=(productData,file)=>{
     axiosInstance.post(productData,file)
 }
+
+export const getProductById=(id)=>{
+    axiosInstance.get(id);
+}
+
+export const getAllProduct=()=>{
+    axiosInstance.get();
+}
+
+export const updateProduct=(id,productData,file)=>{
+    axiosInstance.update(id,productData,file);
+}
+
+export const deleteProdutById=(id)=>{
+    axiosInstance.delete(id);
+}
+
+export const deleteAllProduct=()=>{
+    axiosInstance.delete();
+}
