ccca0c6087b130e0ccd87a126a9c02f6afa13ebd
SyedMuhammadIzaan <muhammadizaan258@gmail.com>
2026-01-23 22:15:19 +0500

    update addToCart api code

--- src/api/addToCard.js
+++ src/api/addToCard.js
@@ -1,21 +1,26 @@
 import axiosInstance from "./axiosInstance"
 
-export const addItemToCart=(itemData,file)=>{
-    axiosInstance.post('/add-to-cart/c/item',itemData,file);
+export const addItemToCart=async (itemData,file)=>{
+    const response=await axiosInstance.post('/add-to-cart/c/item',itemData,file);
+    return response.data;
 }
 
-export const updateItemOfCart=(id,itemData,file)=>{
-    axiosInstance.put(`/add-to-cart/${id}`,itemData,file);
+export const updateItemOfCart=async (id,itemData,file)=>{
+    const response=await axiosInstance.put(`/add-to-cart/${id}`,itemData,file);
+    return response.data;
 }
 
-export const getCartItemById=(id)=>{
-    axiosInstance.get(`/add-to-cart/${id}`)
+export const getCartItemById=async (id)=>{
+    const response=await axiosInstance.get(`/add-to-cart/${id}`);
+    return response
 }
 
-export const deleteCartItemById=(id)=>{
-    axiosInstance.delete(`/add-to-cart/${id}`);
+export const deleteCartItemById=async (id)=>{
+    await axiosInstance.delete(`/add-to-cart/${id}`);
+    return id;
 }
 
-export const deleteAllCartItem=()=>{
-    axiosInstance.delete();
+export const deleteAllCartItem=async ()=>{
+    await axiosInstance.delete();
+    return id;
 }
