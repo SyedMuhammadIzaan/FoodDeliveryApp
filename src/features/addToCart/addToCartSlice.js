214a5203cd2e077482e924ee9a98071a483f378a
SyedMuhammadIzaan <muhammadizaan258@gmail.com>
2026-01-27 23:02:00 +0500

    create reducer for addToCart

---
+++ src/features/addToCart/addToCartSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    items: [],
+    loading: false,
+    error: false,
+}
+const addToCartSlice = createSlice({
+    name: "addToCart",
+    initialState,
+    reducers: {
+        createItemIntoCart: (state, action) => {
+            const product = action.payload;
+
+            const exists = state.items.find(
+                (item) => item.id === product.id
+            );
+            if (!exists) {
+                state.items = product;
+            }
+        },
+        clearCartItem: (state) => {
+            state.items = []
+        },
+        deleteCartItem: (state, action) => {
+            const productIndex = state.items.findIndex((item) => item.id === action.payload.id)
+            if (productIndex !== -1) state.items.splice(productIndex, 1);
+        }
+    }
+
+})
