834fdeefe5be3a97ed8d81f154b2059afb4c07c5
SyedMuhammadIzaan <muhammadizaan258@gmail.com>
2026-01-30 21:09:48 +0500

    create a updateDelivery thunk

--- src/thunk/deliveryInfoThunks.js
+++ src/thunk/deliveryInfoThunks.js
@@ -1,13 +1,25 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { newDelivery } from "../api/deliveryinfo.api";
+import { newDelivery, updateDeliveryDataById } from "../api/deliveryinfo.api";
 
 export const createDelivery=createAsyncThunk(
     "delivery/c/",
     async(deliveryData,{rejectWithValue})=>{
         try {
-            return newDelivery(deliveryData);
+            return await newDelivery(deliveryData);
         } catch (error) {
             return rejectWithValue(error.response?.data);
         }
     }
 )
+
+export const updateDelivery=createAsyncThunk(
+    "delivery/u/",
+    async (id,deliveryData,{rejectWithValue})=>{
+        try {
+            return await updateDeliveryDataById(id,deliveryData)
+        } catch (error) {
+            return rejectWithValue(error.response?.data);
+        }
+    }
+)
+
